29bcce0f5f0e490ca630e0894ec00110d1c36a40
Kevin Shitaye <it.kevin.shitaye@gmail.com>
2020-09-23 22:32:30 +0300

    finished email sending functonality....now started working on the inbox view

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,11 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
 
+document.addEventListener('DOMContentLoaded', function() {
+  
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-
+  document.querySelector('#compose-form').addEventListener('submit', send)
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +31,46 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  if (mailbox === 'inbox'){
+    fetch('emails/inbox')
+    .then(response => response.json())
+    .then(emails => {
+      // Printing result to console
+      console.log(emails)
+      const mails = document.createElement('div')
+      // creating the inbox mailbox
+      document.querySelector('#emails-view').append()
+
+    })
+  }
+
+}
+
+
+
+
+function send() {
+  // Getting the values
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  
+  // send email via the push
+  fetch('emails',{
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Printing it to the console
+    console.log(result)
+    // redirecting to Sent
+    load_mailbox('sent')
+  });
+  return false
 }
