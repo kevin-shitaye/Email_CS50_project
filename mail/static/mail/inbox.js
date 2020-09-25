6cd091e5dd7bc71ee7454abecb527564170f3490
Kevin Shitaye <it.kevin.shitaye@gmail.com>
2020-09-25 21:34:11 +0300

    finished the mailbox views and view email functionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-  document.querySelector('#compose-form').addEventListener('click', send)
+  document.querySelector('#compose-form').addEventListener('click', send);
+  
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -16,6 +17,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#view').style.display = "none";
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -28,19 +30,35 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#view').style.display = "none";
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+
+  
   // creating a string litral for mailbox to be fetched
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
     // ... do something else with emails ...
     emails.forEach(email => {
-      div = document.createElement('div')
-      div.innerHTML = `From: ${email.sender}. Subject: ${email.subject}`
-      document.querySelector('#emails-view').append(div)
+      div = document.createElement('div');
+      div.classList.add("mailboxes");
+      div.id = email.id;
+      div.addEventListener('click', email_boxes);
+      
+      if (mailbox === "sent"){
+        div.innerHTML = `TO <strong>:  ${email.recipients} : </strong>${email.subject} <div>${email.timestamp}</div>`;  
+      }
+      else{
+        if (email.read === false){
+          div.style.backgroundColor = "rgb(230, 230, 230)"
+        }
+        div.innerHTML = `<strong>${email.sender} : </strong>${email.subject} <div>${email.timestamp}</div>`;
+      }
+      
+      document.querySelector('#emails-view').append(div);
     });
 
   });
@@ -70,4 +88,30 @@ function send() {
     load_mailbox('sent')
   });
   return false
+}
+
+function email_boxes(e){
+
+
+
+  fetch(`/emails/${e.target.id}`)
+  .then(response => response.json())
+  .then(email =>{
+    document.querySelector('#view_from').innerHTML = `From: ${email.sender} : ${email.timestamp}`;
+    document.querySelector('#view_subject').innerHTML = `Subject ${email.subject}`;
+    document.querySelector('#view_body').innerHTML = email.body;
+
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#view').style.display = "block";
+  });
+
+  fetch(`/emails/${e.target.id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+
+  })
+  
 }
